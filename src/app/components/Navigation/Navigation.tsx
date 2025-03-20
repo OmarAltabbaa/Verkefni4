335b00d100b4fe9ac6be34b3760eec5572de33af
OmarAltabbaa <https://github.com/OmarAltabbaa/vef2-2022-verkefni>
2025-03-20 19:32:46 +0000

    almost done

---
+++ src/app/components/Navigation/Navigation.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import styles from './Navigation.module.css'
+
+
+  export default function Navigation() {
+    return (
+        <nav className={styles.nav}>
+          <ul>
+            <li><Link href="/">Forsíða</Link></li>
+            <li><Link href="/flokkar">Flokkar</Link> </li>
+          </ul>
+        </nav>
+      
+      
+    );
+  }
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+import Navigation from "./components/Navigation/Navigation";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <Navigation />
+      <h1>Verkefni 4 </h1>
+      <h2></h2>
+    </div>
+  );
+}
